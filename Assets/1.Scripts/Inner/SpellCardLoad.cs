3bf1dd9cfd949a0027e3fd70e66bee8c073a2dd8
PureAmaya <gkoishi0514@gmail.com>
2022-05-11 12:31:27 +0800

    加了几个插件

---
+++ Assets/1.Scripts/Inner/SpellCardLoad.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YamlDotNet.Serialization;
+
+public class SpellCardLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       // var data =
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
